15eb2962a015ed9f9cb2f8a6e5a76b4cbc1707aa
Jonathan Chou <jonathanchou@Jonathans-MacBook-Pro-2.local>
2014-11-11 13:27:43 -0800

    Initial Commit

---
+++ LostCharacters/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LostCharacters
+//
+//  Created by Jonathan Chou on 11/11/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
